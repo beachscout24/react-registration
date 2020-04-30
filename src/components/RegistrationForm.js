bc8fd45e6524a4700f865c6e5e5319c50297468e
Eric Clarke <eric.w.clarke@gmail.com>
2020-04-29 22:53:01 -0400

    Availity - Form Creation

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,59 +1,26 @@
-import React, {Component} from 'react';
-import "../App.css"
+import React, { Component } from "react";
+import "../App.css";
 
 class RegistrationForm extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      npiNumber: "",
+      busAddress: "",
+      telephone: "",
+      email: "",
+    };
+  }
 
-        this.state = {
-            firstName: '',
-            lastName: '',
-            npiNumber: '',
-            busAddress: '',
-            telephone: '',
-            email: ''
-        }
-    }
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-    handleFirstNameChange = event => {
-        this.setState({
-            firstName: event.target.value
-        });
-    }
-
-    handleLastNameChange = event => {
-        this.setState({
-            lastName: event.target.value,
-        })
-    }
-
-    handleNPIChange = event => {
-        this.setState({
-            npiNumber: event.target.value,
-        })
-    }
-
-    handleAddressChange = event => {
-        this.setState({
-            busAddress: event.target.value,
-        })
-    }
-
-    handleTelephoneChange = event => {
-        this.setState({
-            telephone: event.target.value,
-        })
-    }
-
-    handleEmailChange = event => {
-        this.setState({
-            email: event.target.value,
-        })
-    }
-
-    handleSubmit = event => {
-        alert(`
+  handleSubmit = (event) => {
+    alert(`
                 ${this.state.firstName}\n
                 ${this.state.lastName}\n
                 ${this.state.npiNumber}\n
@@ -61,41 +28,76 @@ class RegistrationForm extends Component {
                 ${this.state.telephone}\n
                 ${this.state.email}
             `);
-        event.preventDefault();
-    }
+    event.preventDefault();
+  };
 
-    render() {
-        const {firstName, lastName, npiNumber, busAddress, telephone, email} = this.state
-        return (
-            <form onSubmit={this.handleSubmit} method="POST" action="" id="form-submit">
-                <div>
-                    <label htmlFor="firstName">First Name</label>
-                    <input name="firstName" value={firstName} type="text" onChange={this.handleFirstNameChange}/></div>
-                <div>
-                    <label htmlFor="lastName" >Last Name</label>
-                    <input name="lastName" value={lastName} onChange={this.handleLastNameChange}/>
-                </div>
-                <div>
-                    <label htmlFor="npiNumber" >Npi Number</label>
-                    <input name="npiNumber" value={npiNumber} onChange={this.handleNPIChange}/>
-                </div>
-                <div>
-                    <label htmlFor="busAddress" >Business Address</label>
-                    <input name="busAddress" value={busAddress} onChange={this.handleAddressChange}/>
-                </div>
-                <div>
-                    <label htmlFor="telephone" >Telephone</label>
-                    <input name="telephone" value={telephone} onChange={this.handleTelephoneChange}/>
-                </div>
-                <div>
-                    <label htmlFor="email" >Email</label>
-                    <input name="email" value={email} onChange={this.handleEmailChange}/></div>
-                <div>
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
-        );
-    }
+  render() {
+    const {
+      firstName,
+      lastName,
+      npiNumber,
+      busAddress,
+      telephone,
+      email,
+    } = this.state;
+    return (
+      <form
+        onSubmit={this.handleSubmit}
+        method="POST"
+        action=""
+        id="form-submit"
+      >
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            name="firstName"
+            value={firstName}
+            type="text"
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            name="lastName"
+            value={lastName}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="npiNumber">Npi Number</label>
+          <input
+            name="npiNumber"
+            value={npiNumber}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="busAddress">Business Address</label>
+          <input
+            name="busAddress"
+            value={busAddress}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="telephone">Telephone</label>
+          <input
+            name="telephone"
+            value={telephone}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input name="email" value={email} onChange={this.handleChange} />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default RegistrationForm;
